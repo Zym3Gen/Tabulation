6d2b1b600211d5ef05d0ceda003522f39c89470d
Zym3Gen <g1992s@139.com>
2020-10-27 10:32:33 +0800

    fix typo

--- config.go
+++ config.go
@@ -4,7 +4,6 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth/sso"
 )
